9a00df70df5fd79903929faaf2112d495db4c61b
dims572 <114567853+dims572@users.noreply.github.com>
2025-03-26 22:33:45 +0700

    Add files via upload

---
+++ tugas6/main.js
@@ -0,0 +1,21 @@
+import { index, store, destroy } from "./controller.js";
+
+const main = () => {
+  console.log("Sebelum Menambahkan Data:");
+  index();
+
+  // Menambahkan 2 data baru
+  store({ nama: "Data 11", umur: 30, alamat: "Jl. Data 11", email: "data11@gmail.com" });
+  store({ nama: "Data 12", umur: 31, alamat: "Jl. Data 12", email: "data12@gmail.com" });
+
+  console.log("\nSetelah Menambahkan Data:");
+  index();
+
+  // Menghapus satu data terakhir
+  destroy();
+
+  console.log("\nSetelah Menghapus Satu Data:");
+  index();
+};
+
+main();
